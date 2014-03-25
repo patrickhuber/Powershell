3670ce1c1ea1060d2d07d3c2fabcf87f2ce15056
Patrick Huber <prhuber@gmail.com>
2014-03-24 22:46:49 -0400

    initial revision

---
+++ Tfs/Tfs-Common.ps1
@@ -0,0 +1,3 @@
+Add-Type -AssemblyName @("Microsoft.TeamFoundation.Client, Version=11.0.0.0, Culture=neutral, PublicKeyToken=b03f5f7f11d50a3a",
+    "Microsoft.TeamFoundation.Common, Version=11.0.0.0, Culture=neutral, PublicKeyToken=b03f5f7f11d50a3a",
+    "Microsoft.TeamFoundation, Version=11.0.0.0, Culture=neutral, PublicKeyToken=b03f5f7f11d50a3a")

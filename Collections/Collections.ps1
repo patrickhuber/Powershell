479b6c7cd72a18d246ce9825962d814a9f170bf3
patrick huber <prhuber@gmail.com>
2015-10-23 14:03:38 -0400

    adds collections ps script and adds markdown site template

---
+++ Collections/Collections.ps1
@@ -0,0 +1,20 @@
+﻿function New-SortedDictionary([type]$keyType, [type]$valueType)
+{
+    $base = [System.Collections.Generic.SortedDictionary``2]
+    $ct = $base.MakeGenericType(($keyType, $valueType))
+    New-Object $ct
+}
+
+function New-GenericDictionary([type]$keyType, [type]$valueType)
+{
+    $base = [System.Collections.Generic.Dictionary``2]
+    $ct = $base.MakeGenericType(($keyType, $valueType))
+    New-Object $ct
+}
+
+function New-GenericList([type]$type)
+{
+    $base = [System.Collections.Generic.List``1]
+    $qt = $base.MakeGenericType(@($type))
+    New-Object $qt
+}
---
+++ Markdown/documentation/test.md
@@ -0,0 +1,14 @@
+# h1
+## h2
+### h3
+#### h4
+##### h5
+###### h6
+
+* bullet 1
+* bullet 2
+* bullet 3
+
+1. number 1
+2. number 2
+3. number 3
